4938de047aea13203f4830171fe6949a277caf00
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-25 22:38:24 -0300

    Retirada de espaços no codigo

--- SalarioCargos/Program.cs
+++ SalarioCargos/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SalarioCargos
 {
@@ -12,40 +8,38 @@ namespace SalarioCargos
         {
             float salario;
 
-            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.WriteLine("Informe o salário: ");
             Console.ResetColor();
 
             salario = float.Parse(Console.ReadLine());
 
-            if (salario == 1500.00f )
+            if (salario == 1500.00f)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Cargo: Estagiário.");
                 Console.ResetColor();
             }
-            
-            else if(salario >= 2000.00f && salario <= 2400.00f) 
+            else if (salario >= 2000.00f && salario <= 2400.00f)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Cargo: Professor Nível 1.");
                 Console.ResetColor();
             }
-
-            else if (salario > 2400.00f && salario <= 3000.00f) 
+            else if (salario > 2400.00f && salario <= 3000.00f)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Cargo: Professor Nível 2.");
                 Console.ResetColor();
             }
-            else if (salario > 4250.92f && salario <= 5000.00f) 
+            else if (salario > 4250.92f && salario <= 5000.00f)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Cargp: Diretor.");
                 Console.ResetColor();
             }
-            else 
+            else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Este salário não existe no quadro de funcionários da nossa escola.");
@@ -53,10 +47,6 @@ namespace SalarioCargos
             }
 
             Console.ReadKey();
-
-
-
-
 
         }
     }
